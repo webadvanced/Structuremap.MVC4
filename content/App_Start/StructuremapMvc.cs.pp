1fbb07677f6cf350cce5f837fe674efad521dbf0
Web Advanced <paul.barton@webadvanced.com>
2012-03-06 12:30:57 -0800

    initial load

---
+++ content/App_Start/StructuremapMvc.cs.pp
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using StructureMap;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.StructuremapMvc), "Start")]
+
+namespace $rootnamespace$.App_Start {
+    public static class StructuremapMvc {
+        public static void Start() {
+            var container = (IContainer) IoC.Initialize();
+            DependencyResolver.SetResolver(new SmDependencyResolver(container));
+			// this override is needed because WebAPI is not using DependencyResolver to build controllers 
+			GlobalConfiguration.Configuration.ServiceResolver.SetResolver(
+			DependencyResolver.Current.GetService, 
+			DependencyResolver.Current.GetServices);
+        }
+    }
+}
