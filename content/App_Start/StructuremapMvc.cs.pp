11890551d77f2cb11de98b0839b007e57f7b822e
Web Advanced <paul.barton@webadvanced.com>
2012-08-30 10:10:17 -0700

    Updated to work with MVC4 RTM

--- content/App_Start/StructuremapMvc.cs.pp
+++ content/App_Start/StructuremapMvc.cs.pp
@@ -1,17 +1,15 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
 using StructureMap;
-using StructureMap.ServiceLocatorAdapter;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.StructuremapMvc), "Start")]
 
 namespace $rootnamespace$.App_Start {
     public static class StructuremapMvc {
         public static void Start() {
-            var container = (IContainer) IoC.Initialize();
-            DependencyResolver.SetResolver(new SmDependencyResolver(container));
-			// this override is needed because WebAPI is not using DependencyResolver to build controllers 
-			GlobalConfiguration.Configuration.ServiceResolver.SetResolver(new StructureMapServiceLocator(container));
+			IContainer container = IoC.Initialize();
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new StructureMapDependencyResolver(container);
         }
     }
 }
---
+++ content/DependencyResolution/StructureMapDependencyResolver.cs.pp
@@ -0,0 +1,42 @@
+﻿using System.Web.Http.Dependencies;
+using StructureMap;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// The structure map dependency resolver.
+    /// </summary>
+    public class StructureMapDependencyResolver : StructureMapDependencyScope, IDependencyResolver
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StructureMapDependencyResolver"/> class.
+        /// </summary>
+        /// <param name="container">
+        /// The container.
+        /// </param>
+        public StructureMapDependencyResolver(IContainer container)
+            : base(container)
+        {
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The begin scope.
+        /// </summary>
+        /// <returns>
+        /// The System.Web.Http.Dependencies.IDependencyScope.
+        /// </returns>
+        public IDependencyScope BeginScope()
+        {
+            IContainer child = this.Container.GetNestedContainer();
+            return new StructureMapDependencyResolver(child);
+        }
+
+        #endregion
+    }
+}
